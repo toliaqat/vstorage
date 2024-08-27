aba99862048cc9e6f4d1ca22c7abf404bb153ce5
Touseef Liaqat <touseefliaqat@gmail.com>
2024-08-27 01:04:47 -0700

    Add devnet, xnet and ollinet

--- src/config.js
+++ src/config.js
@@ -2,6 +2,9 @@
 const apiEndpoints = [
   { label: "Mainnet", value: "https://main.api.agoric.net:443" },
   { label: "Emerynet", value: "https://emerynet.api.agoric.net:443" },
+  { label: "Devnet", value: "https://devnet.api.agoric.net:443" },
+  { label: "Xnet", value: "https://xnet.api.agoric.net:443" },
+  { label: "Ollinet", value: "https://ollinet.api.agoric.net:443" },
   { label: "Localhost", value: "http://localhost:1317" },
 ];
 
