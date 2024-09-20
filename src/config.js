8ef0f6dea43e3f55082396ce563cdbaf9e71a3e3
Touseef Liaqat <touseefliaqat@gmail.com>
2024-09-19 17:27:14 -0700

    feat: update api to rpc endpoint in config

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 // config.js
 const apiEndpoints = [
-  { label: "Mainnet", value: "https://main.api.agoric.net:443" },
-  { label: "Emerynet", value: "https://emerynet.api.agoric.net:443" },
-  { label: "Devnet", value: "https://devnet.api.agoric.net:443" },
-  { label: "Xnet", value: "https://xnet.api.agoric.net:443" },
-  { label: "Ollinet", value: "https://ollinet.api.agoric.net:443" },
-  { label: "Localhost", value: "http://localhost:1317" },
+  { label: "Mainnet", value: "https://main.rpc.agoric.net:443" },
+  { label: "Emerynet", value: "https://emerynet.rpc.agoric.net:443" },
+  { label: "Devnet", value: "https://devnet.rpc.agoric.net:443" },
+  { label: "Xnet", value: "https://xnet.rpc.agoric.net:443" },
+  { label: "Ollinet", value: "https://ollinet.rpc.agoric.net:443" },
+  { label: "Localhost", value: "http://localhost:26657" },
 ];
 
 export default apiEndpoints;
